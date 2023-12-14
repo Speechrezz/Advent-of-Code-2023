b7ede43f96b77fb292fe1d7aea2b42fa9ca2ba6f
Speechrezz <thespeechlessmusic@gmail.com>
2023-12-13 19:59:20 -0800

    Completed day 4 part 2

--- Day 4/part2/src/main.rs
+++ Day 4/part2/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-fn get_points_in_line(line: &str) -> i32 {
+fn get_win_count(line: &str) -> i32 {
     let start_idx = line.chars().position(|c| c == ':').unwrap();
     let separator_idx = line.chars().position(|c| c == '|').unwrap();
     let winning_numbers: Vec<i32> = line[start_idx + 1..separator_idx]
@@ -8,27 +8,33 @@ fn get_points_in_line(line: &str) -> i32 {
         .map(|num| num.parse::<i32>().unwrap())
         .collect();
 
-    let mut score = 1;
+    let mut sum = 0;
 
     line[separator_idx + 1..]
         .split_whitespace()
         .for_each(|num| {
             let num = num.parse::<i32>().unwrap();
             if winning_numbers.contains(&num) {
-                score *= 2;
+                sum += 1;
             }
         });
 
-    score / 2
+    sum
 }
 
 fn main() {
     let input = fs::read_to_string("../input.txt").unwrap();
+    let mut card_counts: Vec<i32> = Vec::new();
+    card_counts.resize(input.lines().count(), 1);
 
-    let mut sum = 0;
-    for line in input.lines() {
-        sum += get_points_in_line(line);
+    for (i, line) in input.lines().enumerate() {
+        let win_count = get_win_count(line);
+        let end_range = (i + 1 + win_count as usize).min(card_counts.len());
+        let current_count = card_counts[i];
+        for count in card_counts[i + 1..end_range].iter_mut() {
+            *count += current_count;
+        }
     }
 
-    println!("Sum: {}", sum);
+    println!("Sum: {}", card_counts.iter().sum::<i32>());
 }
